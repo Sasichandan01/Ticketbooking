7fd21e2fd88a531ba69cd16d7ad508b777d6066c
Sasi chandan <bhargavsasichandan.com>
2024-03-18 19:13:26 +0530

    first

--- router/auth-router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router=express.Router();
-const { home, contact } = require("../controller/auth-controller");
-router.route("/").get(home);
-router.route("/contact").post(contact);
-module.exports=router
